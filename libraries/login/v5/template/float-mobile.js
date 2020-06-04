4330610ce5ac5476a0cc781de6cb7ff5fbd72738
冯国清 <master@rebeta.cn>
2020-06-04 17:29:50 +0800

    feat : 修正注释;

--- libraries/login/v5/template/float-mobile.js
+++ libraries/login/v5/template/float-mobile.js
@@ -17,8 +17,8 @@ document.getElementsByTagName("chaos-v5")[0].innerHTML += `
 
 // 发生滚动后显示悬浮
 window.onscroll = function () {
-  //为了保证兼容性，这里取两个值，哪个有值取哪一个
-  //scrollTop就是触发滚轮事件时滚轮的高度
+  // 为了保证兼容性，这里取两个值，哪个有值取哪一个
+  // scrollTop就是触发滚轮事件时滚轮的高度
   var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
   console.log("滚动距离" + scrollTop);
   // 判断是否大于屏幕高度的四分之一
