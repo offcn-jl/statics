96c62a71e9081cadaa960e509560e85d80fa7cc0
Apple <3422924002@qq.com>
2020-11-26 09:59:38 +0800

    feat: 修改登录框的样式;

--- libraries/login/v6/template/phone.js
+++ libraries/login/v6/template/phone.js
@@ -7,22 +7,23 @@
             <span class="hlpt-current">中 公 教 育</span>
         </div>
         <div class="hlp-middle">
-            <input type="text" id="offcn-sso-phone" placeholder="手机号码" autocomplete="off">
+           <div class="hlbj">
+              <img src="http://jl.offcn.com/zg/ty/images/bd_icon1.png">   
+              <input type="text" id="offcn-sso-phone" placeholder="手机号码" autocomplete="off">
+            </div>   
             <div class="offcn-sso-tips"></div>
-            <div class="offcn-sso-code-line">
-                <input type="text" id="offcn-sso-code" placeholder="验证码" required="" autocomplete="off">
-                <div id="offcn-sso-get-code">获取验证码</div>
-            </div>
+            <div class="hlbj">
+                <div class="offcn-sso-code-line">
+                    <img src="http://jl.offcn.com/zg/ty/images/bd_icon2.png"> 
+                    <input type="text" id="offcn-sso-code" placeholder="验证码" required="" autocomplete="off">
+                    <div id="offcn-sso-get-code">获取验证码</div>
+                </div>
+            </div>    
             <div class="hlpm">
                 <input type="button" value="登 陆" id="offcn-sso-submit" class="submintBtn">
             </div>
             <input type="text" id="offcn-sso-location" value="" readonly style="text-align: center;border: 0px;font-size: 15px;height: 25px;color: gray;">
         </div>
-        <div class="hlp-bottom">
-            <h3></h3>
-            关注中公教育，获取新鲜考试资讯与备考服务<br>免费咨询答疑，我们会在5分钟内给您答复
-            <br><br>
-            <a href="#" target="_blank" rel="nofollow" class="ad-button">点击关注</a>
-        </div>
+        <div class="hlp-bottom"></div>
     </div>
 </div>`;
