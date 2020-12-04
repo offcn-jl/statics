632dd6fcffe4497422ebabf718e039d40a54ebd4
冯国清 <master@rebeta.cn>
2020-12-04 13:36:18 +0800

    feat: 更新版本号;

--- libraries/login/v5/main.js
+++ libraries/login/v5/main.js
@@ -3,7 +3,7 @@ var Chaos = {
     // 模块基本信息
     Product: {
         Name: "单点登录模块 ( Single Sign On Module )",
-        Ver: "20200617"
+        Ver: "20201204"
     },
     // 公共函数
     Functions: {
